8a16166e5b206c2a4a1f22f73c50c2b6977e7d77
IceBerg <www.favs2@gmail.com>
2017-03-23 17:45:20 +0100

    Oprettet projekt og har brug for hjælp til at rette en fejl

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows.Forms;
+using Google.GData.Client;
+using Google.GData.Extensions;
+using Google.GData.Spreadsheets;
+using SheetsQuickstart;
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        String[] Pvs = File.ReadAllText(@"C:\Users\Mc IceBerg\AppData\Roaming\StudyLock\Process.csv").Split(',');
+        Timer timer1;
+        int minutes;
+        int hours;
+        bool hasExited;
+        Process[] process;
+        String[] Programs =
+        {
+            "Din mor"
+        };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            info.Text = textBox1.Text + " Hours, " + textBox2.Text + " Minues";
+            try
+            {
+                hours = Int32.Parse(textBox1.Text);
+            }
+            catch (FormatException c)
+            {
+                Console.WriteLine(c.Message);
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            info.Text = textBox1.Text + " Hours, " + textBox2.Text + " Minutes";
+            try
+            {
+                minutes = Int32.Parse(textBox2.Text);
+            }
+            catch (FormatException c)
+            {
+                Console.WriteLine(c.Message);
+            }
+            if (minutes > 60)
+            {
+                hours++;
+                minutes = minutes - 60;
+                textBox2.Text = "" + minutes;
+                textBox1.Text = "" + hours;
+            }
+
+        }
+
+        private void info_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            textBox1.Visible = false;
+            textBox2.Visible = false;
+            label1.Visible = false;
+            label2.Visible = false;
+            foreach (String i in Pvs)
+            {
+                Console.WriteLine(i);
+            }
+            MessageBox.Show("Remember to save everything before starting");
+            KillPrograms(Pvs);
+            timer1 = new System.Windows.Forms.Timer();
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Interval = 1000; // 1 second
+            timer1.Start();
+            Values.Text = hours.ToString() + " Hours, " + minutes + " minutes";
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            minutes--;
+            Values.Text = hours.ToString() + " Hours, " + minutes + " minutes";
+            if (minutes == 0 && hours != 0)
+            {
+                hours--;
+                minutes = 60;
+            }
+            if (minutes == 0 && hours == 0)
+            {
+                timer1.Stop();
+                Values.Text = hours.ToString() + " Hours, " + minutes + " minutes";
+            }
+        }
+        public void KillPrograms(String[] processes)
+        {
+            process = Process.GetProcesses();
+            Console.WriteLine(process[4]);
+            Console.WriteLine(process.Length);
+        // Kører igennem alle processer
+        for(int i = 0; i < processes.Length; i++)
+            {
+                // Checker hver gang loopet kører igennem
+                foreach(System.Diagnostics.Process process in System.Diagnostics.Process.GetProcesses())
+                {
+                    Console.WriteLine("Checked!");
+                    // "omdøber" process navnene til lower case så det kan tjekkes
+                    if(process.ProcessName.ToLower() == processes[i].ToLower())
+                    {
+                        process.Kill();
+                        Console.WriteLine("Killed");
+                    }
+                }
+                Console.WriteLine("Runned through all process");
+            }
+            // lortet virker ikke, hvis du kan regne den ud kan du se i debug hvorfor
+        }
+        void Form_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                hasExited = true;
+                killProg();
+            }
+
+            if (e.CloseReason == CloseReason.WindowsShutDown)
+            {
+                hasExited = true;
+                killProg();
+            }
+        }
+        public bool killProg()
+        {
+            if(hasExited)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Program.cs
@@ -0,0 +1,97 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WindowsFormsApplication1;
+using System.Windows.Forms;
+
+namespace SheetsQuickstart
+{
+    class Program
+    {
+        // If modifying these scopes, delete your previously saved credentials
+        // at ~/.credentials/sheets.googleapis.com-dotnet-quickstart.json
+        static string[] Scopes = { SheetsService.Scope.SpreadsheetsReadonly };
+        static string ApplicationName = "Google Sheets API .NET Quickstart";
+        static void Main(string[] args)
+
+        {
+            UserCredential credential;
+
+            using (var stream =
+                new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = System.Environment.GetFolderPath(
+                    System.Environment.SpecialFolder.Personal);
+                credPath = Path.Combine(credPath, ".credentials/sheets.googleapis.com-dotnet-quickstart.json");
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Google Sheets API service.
+            var service = new SheetsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            // Define request parameters.
+            String spreadsheetId = "1isflrahjYmtYmV07fWW7vv_TNOIaqoPeHiG1TeCHQgI";
+            String range = "Ark1!A1:A999";
+            SpreadsheetsResource.ValuesResource.GetRequest request =
+                    service.Spreadsheets.Values.Get(spreadsheetId, range);
+
+            // Prints the names and majors of students in a sample spreadsheet:
+            // https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+            ValueRange response = request.Execute();
+            IList<IList<Object>> values = response.Values;
+            StringBuilder sb = new StringBuilder();
+            string delimiter = ",";
+            if (values != null && values.Count > 0)
+            {
+                Console.WriteLine("Name, Major");
+                foreach (var row in values)
+                {
+                    // Print columns A and E, which correspond to indices 0 and 4.
+                    try
+                    {
+                        // Console.WriteLine("{0}, {1}", row[0], row[1]);
+                        Console.WriteLine(row[0]);
+                        sb.AppendLine(string.Join(delimiter, row[0]));
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Out of range");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("No data found.");
+            }
+            string filePath = @"C:\Users\Mc IceBerg\AppData\Roaming\StudyLock\Process.csv";
+            File.WriteAllText(filePath, sb.ToString());
+            var main_form = new Form1();
+            main_form.Show();
+            Application.Run();
+            if(main_form.killProg())
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
